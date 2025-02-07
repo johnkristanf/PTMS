96b356146c9da9675123ccbe4daa6fddccb29a8e
John Kristan Torremocha <johnkristan01@gmail.com>
2025-02-07 10:00:15 +0800

    feat: fixing bugs again

--- client/src/components/pdfs/completionStyles/electrical/box/box2.tsx
+++ client/src/components/pdfs/completionStyles/electrical/box/box2.tsx
@@ -5,7 +5,7 @@ import box1Image from '/img/Completion/Electrical/box2.png';
 export function PermitBodyBox2() {
     return (
         <View style={styles.imageContainer}>
-              <Image source={{ uri: box1Image }} />
+              <Image source={box1Image } />
         </View>
     );
 }
--- client/src/components/pdfs/completionStyles/electrical/box/box3.tsx
+++ client/src/components/pdfs/completionStyles/electrical/box/box3.tsx
@@ -5,7 +5,7 @@ import box1Image from '/img/Completion/Electrical/page2.png';
 export function PermitBodyBox3() {
     return (
         <View style={styles.imageContainer}>
-              <Image source={{ uri: box1Image }} />
+              <Image source={ box1Image } />
         </View>
     );
 }
